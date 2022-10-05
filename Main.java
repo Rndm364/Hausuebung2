64bd864ad24166468c355b423f11e0076e348b6c
Dominik Simmer <dominik@MacBook-Pro-von-Dominik.local>
2022-10-05 23:21:10 +0200

    working on output in main

--- Main.java
+++ Main.java
@@ -1,6 +1,42 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         NumberTester numberTester = new NumberTester("Numbers.txt");
         numberTester.testFile();
+
+        System.out.println("Choose calculator");
+        System.out.println("1 - Rational calculator");
+        System.out.println("2 - Vector calculator");
+        System.out.println("3 - Complex calculator");
+        System.out.println("4 - Exit programm");
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        Number number1 = new Number();
+        Number number2 = new Number();
+        switch(number){
+            case 1:
+                System.out.println("Please enter the first number");
+                number1.setA(s.nextInt());
+                System.out.println("Please enter the second number");
+                number2.setB(s.nextInt());
+
+            case 2:
+            case 3:
+
+            case 4: System.exit(0); break;
+            default:
+        }
+        operationTypes();
     }
+    private static void operationTypes(){
+        System.out.println("Choose operation");
+        System.out.println("1 - add");
+        System.out.println("2 - subtract");
+        System.out.println("3 - multiply");
+        System.out.println("4 - divide");
+        System.out.println("5 - enter numbers again");
+    }
+
 }
--- NumberTester.java
+++ NumberTester.java
@@ -29,43 +29,12 @@ public class NumberTester {
     public void testFile(){
 
         try {
-            Scanner s = null;
-
-            String strCurrentLine;
-
-            s = new Scanner(new File(fileName));
-
+            Scanner s = new Scanner(new File(fileName));
             int zahl = Integer.parseInt(s.nextLine());
-
                 for(int i = 0; i < zahl; i++ ) {
                     String line = s.nextLine();
                     String[] parts = line.split(" ");
-                    switch (Integer.parseInt(parts[0])) {
-                        case 1:
-                            if (oddTester.testNumber(Integer.parseInt(parts[1]))) {
-                                System.out.println("ODD");
-                                break;
-                            } else {
-                                System.out.println("EVEN");
-                            }
-                            break;
-                        case 2:
-                            if (primeTester.testNumber(Integer.parseInt(parts[1]))) {
-                                System.out.println("PRIME");
-                                break;
-                            } else {
-                                System.out.println("NO PRIME");
-                            }
-                            break;
-                        case 3:
-                            if (palindromeTester.testNumber(Integer.parseInt(parts[1]))) {
-                                System.out.println("PALINDROME");
-                                break;
-                            } else {
-                                System.out.println("NO PALINDROME");
-                            }
-                            break;
-                    }
+                    System.out.println(line);
                 }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
